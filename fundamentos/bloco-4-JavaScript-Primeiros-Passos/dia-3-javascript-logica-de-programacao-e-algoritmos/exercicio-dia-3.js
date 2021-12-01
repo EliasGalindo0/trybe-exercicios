b6b1b0d5beb8c5ee03718626f76bcf7c4b62ea45
Elias <eliasgalindo@gmail.com>
2021-11-30 22:00:38 -0300

    Correçao de arquivo

--- fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-dia-3.js
+++ fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-dia-3.js
@@ -19,36 +19,36 @@ let posicao = n;
 // };
 
 // Agora inverta o lado do triângulo. 
-// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-//     if (columnIndex < posicao) {
-//       square = square + " "; 
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < posicao) {
+      square = square + " "; 
 
-//       } else {
-//         square = square + symbol;
-//       }
-//     }
-//     console.log(square)
-//     square = " ";
-//     posicao -=1;
-// };
+      } else {
+        square = square + symbol;
+      }
+    }
+    console.log(square)
+    square = " ";
+    posicao -=1;
+};
 
 //Depois, faça uma pirâmide com n asteriscos de base.
 
-let midOfMatrix = (n + 1) / 2;
-let controlLeft = midOfMatrix;
-let controlRight = midOfMatrix;
+// let midOfMatrix = (n + 1) / 2;
+// let controlLeft = midOfMatrix;
+// let controlRight = midOfMatrix;
 
-for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-    if (columnIndex > controlLeft && columnIndex < controlRight) {
-      square = square + symbol;
-    } else {
-      square = square + ' ';
-    }
-  }
-  console.log(square);
-  square = '';
-  controlRight += 1;
-  controlLeft -= 1
-};
+// for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex > controlLeft && columnIndex < controlRight) {
+//       square = square + symbol;
+//     } else {
+//       square = square + ' ';
+//     }
+//   }
+//   console.log(square);
+//   square = '';
+//   controlRight += 1;
+//   controlLeft -= 1
+// };
