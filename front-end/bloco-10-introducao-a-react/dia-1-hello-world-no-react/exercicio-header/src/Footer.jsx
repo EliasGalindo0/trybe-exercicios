e37ef65eaf47fca506ae068c9d28b1707c7a5fba
Elias Galindo <eliasgalindo@pop-os.localdomain>
2022-02-18 14:15:26 -0300

    Time added

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-header/src/Footer.jsx
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-header/src/Footer.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-      class Footer extends React.Component {
-        render() {
-          return (
-            <footer className='footer'>
+const tick = () => {
+  const element = (
+      <h2>It is {new Date().toLocaleTimeString('pt', { hour12: true })}.</h2>
+  );
+  return element;
+}
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className='footer'>
               <h1>E isso é só o começo...</h1>
+              {tick()}
             </footer>
           )
         }
       }
-
+      
+      setInterval(tick, 1000);
       export default Footer;
+      
