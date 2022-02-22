3edf94829d4e04e5548c96a31747f833b3df4fb9
EliasGalindo0 <eliasgalindo@gmail.com>
2022-02-22 20:04:41 -0300

    Finaliza exercicio password

--- front-end/bloco-10-introducao-a-react/Componentes com estado e eventos/exercicios-11.1/src/components/Password.jsx
+++ front-end/bloco-10-introducao-a-react/Componentes com estado e eventos/exercicios-11.1/src/components/Password.jsx
@@ -1,25 +1,44 @@
 import React from "react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 class Password extends React.Component {
   constructor() {
     super()
     this.state = {
       password: '',
+      eyePassword: true
     }
     this.handlePassword = this.handlePassword.bind(this);
+    this.handleEyePassword = this.handleEyePassword.bind(this);
   }
 
   handlePassword(event) {
-    this.setState({
-      password: event.target.value,
-    }, console.log(event.target.value))
+    this.setState(
+    {
+      password: event.target.value
+    })
   };
 
+  handleEyePassword() {
+    this.setState((prevState) => ({
+      eyePassword: !prevState.eyePassword
+    }));
+  }
+
   render() {
+    const { eyePassword } = this.state;
     return (
       <div className="password--input">
         <label htmlFor="pass">Password</label>
-        <input type="password" id="pass" onChange={this.handlePassword}/>
+        <input
+        type={eyePassword ? "text" : "password"} 
+        id="pass" 
+        onChange={this.handlePassword}/>
+        {eyePassword ? (
+          <FaEye className="eye" onClick={this.handleEyePassword} />
+        ) : (
+          <FaEyeSlash className="eye" onClick={this.handleEyePassword} />
+        )}
       </div>
     )
   }
