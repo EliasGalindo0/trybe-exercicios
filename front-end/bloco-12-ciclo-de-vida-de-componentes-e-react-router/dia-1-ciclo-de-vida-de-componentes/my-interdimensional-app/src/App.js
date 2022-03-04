cf2a59a3fc672e59678f2e4b51f2505ad80956f5
EliasGalindo0 <eliasgalindo@gmail.com>
2022-03-03 23:30:30 -0300

    Refactor exercicio

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
@@ -3,41 +3,42 @@ import React, { Component }from 'react';
 import './App.css';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            characters: [],
-        };
-      }
-
-    componentDidMount() {
-      fetch('https://rickandmortyapi.com/api/character')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({characters: data.results})
-      })
+  constructor(){
+    super()
+    this.state = {
+      characters: [],
     }
-
-  render() {
-    const { characters } = this.state;
-    return (
-      <div className="App">
-        <h1>
-          Ricky and Morty Characters:
-        </h1>
-        <div className="body">
-          {characters.map((character) => {
-            return (
-              <div className="container" key={character.name}>
-                <h3>{character.name}</h3>
-                <img src={character.image} alt={character.name}/>
-              </div>
-            )
-          })}
-        </div>
-      </div>
-    );
   }
-}
+  
+  componentDidMount() {
+    const endpoint = 'https://rickandmortyapi.com/api/character';
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        characters: data.results,
+      })
+    })
+  }
+
+    render() {
+      const { characters } = this.state;
+      return (
+        <div className="App">
+        <h1>Rick and Morty</h1>
+        <div className="body">
+        { characters.map(({ name, image }) => {
+          return (
+            <div className="container" key={ name }>
+              <h3>{ name }</h3>
+            <img src={ image } alt={ image } />
+            </div>
+          )
+        }) }
+        </div>
+        </div>
+      )
+    }
+  }
 
 export default App;
